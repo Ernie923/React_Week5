85dc16ea568e287282fae9b2025f9e85804e289d
陳勇涵 <hanchen@chenyonghandeMacBook-Pro.local>
2025-02-24 22:53:32 +0800

    first upload

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+import './assets/all.scss'
+import App from './App.jsx'
+import 'bootstrap'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
